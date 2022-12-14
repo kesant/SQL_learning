05e65ed771b2408bb9d57ee698ba7ad3e4661fe4
kevin santana <kevin_santana120@hotmail.com>
2022-12-13 22:01:58 -0500

    agregado ejercicios de uso de querys en sql

---
+++ practicas_curso/completar_bd_sakila.sql
@@ -0,0 +1,16 @@
+USE SAKILA;
+SELECT*FROM ACTOR ;
+
+
+INSERT INTO SAKILA.ACTOR (first_name, last_name)
+VALUES 
+("Fiorella", "Guelini"), 
+("Liv", "Tyler"),
+("Helen", "Grady"), 
+("Hector", "Troya"), 
+("Hugo", "Balbuena"), 
+("Robert", "Pattinson"),
+("Fred", "Thompson"), 
+("Nuvo", "Milano");
+DESC ACTOR ;
+SELECT*FROM ACTOR ;
